840be6e6b23ce9491b717886a63bb380aa3dc4e9
revolve 810 <joycemutati72@gmail.com>
2023-10-31 12:24:16 +0300

    sample project

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.students;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> selectedItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         list.add("CCS 2355 Architecture");
         list.add("CCS 2356 Manegarial systems");
 
-
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, list);
 
         listView.setAdapter(arrayAdapter);
@@ -41,20 +41,50 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    // clicked Data analysis
-                    startActivity(new Intent(MainActivity.this,DataanalysisActivity.class
-                    ));
-                } else if (position == 1) {
-                    // clicked Knowledge representation
-                    startActivity(new Intent(MainActivity.this,KnowledgeActivity.class
-                    ));
-                } else {
-                    // Handle other items
+                String selectedItem = list.get(position);
 
+                // Check if the item is already selected
+                if (selectedItems.contains(selectedItem)) {
+                    // If selected, remove it from the list
+                    selectedItems.remove(selectedItem);
+                } else {
+                    // If not selected, check the selection limits
+                    if (selectedItems.size() >= 5 && selectedItems.size() < 10) {
+                        selectedItems.add(selectedItem);
+                        // Handle the selected item, you can start a new activity here
+                        handleSelectedItem(selectedItem);
+                    }
                 }
 
+                // Perform any other actions or UI updates based on the selected items
+                updateUI();
             }
         });
+    }
+
+    private void handleSelectedItem(String selectedItem) {
+        // Start a new activity based on the selected item
+        Intent intent;
+        switch (selectedItem) {
+            case " CCS 2345 Data analysis":
+                intent = new Intent(MainActivity.this, DataanalysisActivity.class);
+                break;
+            case " CCS 2346 Knowledge representation":
+                intent = new Intent(MainActivity.this, KnowledgeActivity.class);
+                break;
+            // Add more cases for other items as needed
+            default:
+                intent = null;
+        }
+
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+    private void updateUI() {
+        // Update your UI based on the selected items, if needed
+        // For example, you can show a message or perform some action
+        // based on the selected items.
     }
 }
